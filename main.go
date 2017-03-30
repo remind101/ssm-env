088c9c9ffef17703283a3a1b08c03d6ef9115503
Eric J. Holmes <eric@ejholmes.net>
2017-03-29 22:33:33 -0700

    Add more test coverage

--- main.go
+++ main.go
@@ -17,8 +17,6 @@ import (
 // from AWS Parameter Store.
 const Prefix = "ssm://"
 
-var client = ssm.New(session.New())
-
 func main() {
 	var (
 		decrypt = flag.Bool("with-decryption", false, "Will attempt to decrypt the parameter, and set the env var as plaintext")
@@ -29,8 +27,29 @@ func main() {
 	path, err := exec.LookPath(args[0])
 	must(err)
 
-	must(expandEnviron(*decrypt))
+	var os osEnviron
+	e := &expander{ssm: ssm.New(session.New()), os: os}
+	must(e.expandEnviron(*decrypt))
 	must(syscall.Exec(path, args[0:], os.Environ()))
+}
+
+type ssmClient interface {
+	GetParameters(*ssm.GetParametersInput) (*ssm.GetParametersOutput, error)
+}
+
+type environ interface {
+	Environ() []string
+	Setenv(key, vale string)
+}
+
+type osEnviron int
+
+func (e osEnviron) Environ() []string {
+	return os.Environ()
+}
+
+func (e osEnviron) Setenv(key, val string) {
+	os.Setenv(key, val)
 }
 
 type ssmVar struct {
@@ -38,7 +57,12 @@ type ssmVar struct {
 	parameter string
 }
 
-func expandEnviron(decrypt bool) error {
+type expander struct {
+	ssm ssmClient
+	os  environ
+}
+
+func (e *expander) expandEnviron(decrypt bool) error {
 	// Environment variables that point to some SSM parameters.
 	var ssmVars []ssmVar
 
@@ -46,14 +70,19 @@ func expandEnviron(decrypt bool) error {
 		WithDecryption: aws.Bool(decrypt),
 	}
 
-	for _, envvar := range os.Environ() {
+	names := make(map[string]bool)
+	for _, envvar := range e.os.Environ() {
 		k, v := splitVar(envvar)
 		if strings.HasPrefix(v, Prefix) {
 			// The name of the SSM parameter.
 			parameter := v[len(Prefix):]
-			input.Names = append(input.Names, aws.String(parameter))
+			names[parameter] = true
 			ssmVars = append(ssmVars, ssmVar{k, parameter})
 		}
+	}
+
+	for k := range names {
+		input.Names = append(input.Names, aws.String(k))
 	}
 
 	if len(input.Names) == 0 {
@@ -61,17 +90,13 @@ func expandEnviron(decrypt bool) error {
 		return nil
 	}
 
-	resp, err := client.GetParameters(input)
+	resp, err := e.ssm.GetParameters(input)
 	if err != nil {
 		return err
 	}
 
 	if len(resp.InvalidParameters) > 0 {
-		var parameters []string
-		for _, p := range resp.InvalidParameters {
-			parameters = append(parameters, aws.StringValue(p))
-		}
-		return fmt.Errorf("invalid parameters: %v", parameters)
+		return newInvalidParametersError(resp)
 	}
 
 	values := make(map[string]string)
@@ -80,10 +105,30 @@ func expandEnviron(decrypt bool) error {
 	}
 
 	for _, v := range ssmVars {
-		os.Setenv(v.envvar, values[v.parameter])
+		e.os.Setenv(v.envvar, values[v.parameter])
 	}
 
 	return nil
+}
+
+type invalidParametersError struct {
+	InvalidParameters []string
+}
+
+func newInvalidParametersError(resp *ssm.GetParametersOutput) *invalidParametersError {
+	e := new(invalidParametersError)
+	for _, p := range resp.InvalidParameters {
+		if p == nil {
+			continue
+		}
+
+		e.InvalidParameters = append(e.InvalidParameters, *p)
+	}
+	return e
+}
+
+func (e *invalidParametersError) Error() string {
+	return fmt.Sprintf("invalid parameters: %v", e.InvalidParameters)
 }
 
 func splitVar(v string) (key, val string) {
--- main_test.go
+++ main_test.go
@@ -2,24 +2,152 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
-func TestSSMKey(t *testing.T) {
-	tests := []struct {
-		in  string
-		out string
-	}{
-		{"ssm://prod.secret", "prod.secret"},
-		{"production", ""},
+func TestExpandEnviron_NoSSMParameters(t *testing.T) {
+	os := newFakeEnviron()
+	c := new(mockSSM)
+	e := expander{
+		os:  os,
+		ssm: c,
 	}
 
-	for i, tt := range tests {
-		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
-			out := ssmKey(tt.in)
-			assert.Equal(t, tt.out, out)
-		})
+	decrypt := false
+	err := e.expandEnviron(decrypt)
+	assert.NoError(t, err)
+
+	assert.Equal(t, []string{
+		"SHELL=/bin/bash",
+		"TERM=screen-256color",
+	}, os.Environ())
+
+	c.AssertExpectations(t)
+}
+
+func TestExpandEnviron_SimpleSSMParameter(t *testing.T) {
+	os := newFakeEnviron()
+	c := new(mockSSM)
+	e := expander{
+		os:  os,
+		ssm: c,
 	}
+
+	os.Setenv("SUPER_SECRET", "ssm://secret")
+
+	c.On("GetParameters", &ssm.GetParametersInput{
+		Names:          []*string{aws.String("secret")},
+		WithDecryption: aws.Bool(true),
+	}).Return(&ssm.GetParametersOutput{
+		Parameters: []*ssm.Parameter{
+			{Name: aws.String("secret"), Value: aws.String("hehe")},
+		},
+	}, nil)
+
+	decrypt := true
+	err := e.expandEnviron(decrypt)
+	assert.NoError(t, err)
+
+	assert.Equal(t, []string{
+		"SHELL=/bin/bash",
+		"SUPER_SECRET=hehe",
+		"TERM=screen-256color",
+	}, os.Environ())
+
+	c.AssertExpectations(t)
+}
+
+func TestExpandEnviron_DuplicateSSMParameter(t *testing.T) {
+	os := newFakeEnviron()
+	c := new(mockSSM)
+	e := expander{
+		os:  os,
+		ssm: c,
+	}
+
+	os.Setenv("SUPER_SECRET_A", "ssm://secret")
+	os.Setenv("SUPER_SECRET_B", "ssm://secret")
+
+	c.On("GetParameters", &ssm.GetParametersInput{
+		Names:          []*string{aws.String("secret")},
+		WithDecryption: aws.Bool(false),
+	}).Return(&ssm.GetParametersOutput{
+		Parameters: []*ssm.Parameter{
+			{Name: aws.String("secret"), Value: aws.String("hehe")},
+		},
+	}, nil)
+
+	decrypt := false
+	err := e.expandEnviron(decrypt)
+	assert.NoError(t, err)
+
+	assert.Equal(t, []string{
+		"SHELL=/bin/bash",
+		"SUPER_SECRET_A=hehe",
+		"SUPER_SECRET_B=hehe",
+		"TERM=screen-256color",
+	}, os.Environ())
+
+	c.AssertExpectations(t)
+}
+
+func TestExpandEnviron_InvalidParameters(t *testing.T) {
+	os := newFakeEnviron()
+	c := new(mockSSM)
+	e := expander{
+		os:  os,
+		ssm: c,
+	}
+
+	os.Setenv("SUPER_SECRET", "ssm://secret")
+
+	c.On("GetParameters", &ssm.GetParametersInput{
+		Names:          []*string{aws.String("secret")},
+		WithDecryption: aws.Bool(false),
+	}).Return(&ssm.GetParametersOutput{
+		InvalidParameters: []*string{aws.String("secret")},
+	}, nil)
+
+	decrypt := false
+	err := e.expandEnviron(decrypt)
+	assert.Equal(t, &invalidParametersError{InvalidParameters: []string{"secret"}}, err)
+
+	c.AssertExpectations(t)
+}
+
+type fakeEnviron map[string]string
+
+func newFakeEnviron() fakeEnviron {
+	return fakeEnviron{
+		"SHELL": "/bin/bash",
+		"TERM":  "screen-256color",
+	}
+}
+
+func (e fakeEnviron) Environ() []string {
+	var env sort.StringSlice
+	for k, v := range e {
+		env = append(env, fmt.Sprintf("%s=%s", k, v))
+	}
+	env.Sort()
+	return env
+}
+
+func (e fakeEnviron) Setenv(key, val string) {
+	e[key] = val
+}
+
+type mockSSM struct {
+	mock.Mock
+}
+
+func (m *mockSSM) GetParameters(input *ssm.GetParametersInput) (*ssm.GetParametersOutput, error) {
+	args := m.Called(input)
+	return args.Get(0).(*ssm.GetParametersOutput), args.Error(1)
 }
